558a65fc5a4f58f5a48bdf460d776fbdcc38661b
Mocha <15025153960@163.com>
2018-07-26 14:32:36 +0800

    comment & identifier commit

---
+++ src/com/mocha/se/se_03_comment/TestComment.java
@@ -0,0 +1,19 @@
+package com.mocha.se.se_03_comment;
+
+public class TestComment {
+    /**
+     * 文档注释
+     */
+    public void testDocComment() {
+        System.out.println("Test document comment");
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Test comment");
+        // 单行注释
+        /*
+         * 多行注释
+         */
+    }
+}
---
+++ src/com/mocha/se/se_04_identifier/TestIdentifier.java
@@ -0,0 +1,9 @@
+package com.mocha.se.se_04_identifier;
+
+public class TestIdentifier {
+    public static void main(String[] args) {
+        int var = 1;
+        int 变量1 = 2;
+//        int 12a = 2;
+    }
+}
